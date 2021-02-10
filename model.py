2be6a43c7e486e00a36d32ea52715215af386809
kang <jgkang1210@postech.ac.kr>
2021-02-10 23:55:45 +0900

    conv

--- model.py
+++ model.py
@@ -11,6 +11,16 @@ from sklearn.utils.class_weight import compute_class_weight
 from tqdm import tqdm
 from python_speech_features import mfcc
 
+class Config:
+    def __init__(self, mode='conv', nfilt=26, nfeat=13, nfft='512', rate=16000):
+        self.mode = mode
+        self.nfilt = nfilt
+        self.nfeat = nfeat
+        self.nfft = nfft
+        self.rate = rate
+        self.step = int(rate/10)
+
+
 df = pd.read_csv('instruments.csv')
 df.set_index('fname', inplace=True)
 
@@ -21,9 +31,16 @@ for f in df.index:
 classes = list(np.unique(df.label))
 class_dist = df.groupby(['label'])['length'].mean()
 
+n_samples = 2 * int(df['length'].sum()/0.1)
+prob_dist = class_dist / class_dist.sum()
+choices = np.random.choice(class_dist.index, p=prob_dist)
+
 fig, ax = plt.subplots()
 ax.set_title('Class Distribution', y=1.08)
 ax.pie(class_dist, labels=class_dist.index, autopct='%1.1f%%',
        shadow=False, startangle=90)
 ax.axis('equal')
 plt.show()
+
+config = Config(mode='conv')
+
